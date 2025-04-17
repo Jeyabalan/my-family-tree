038fd65b111d1243320fd7cd8a398b36b6af3029
Jeyabalan T <giribala14@gmail.com>
2025-04-17 11:48:14 +0000

    model popup to add the family member

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import { Button, Form, Input, Modal, Select, Space } from 'antd';
+import { useForm } from 'antd/es/form/Form';
+import React, { useEffect, useState } from 'react';
+
+const { Option } = Select
 
 const App: React.FC = () => {
-  return <h1>Hello from Tamil Family Tree App 🎉</h1>;
+  const [form] = useForm()
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const [parentList, setParentList] = useState<string[]>([])
+
+  const familyTree = {
+    headDetails: { name: "MuthuKamachi Pillai", imageUrl: '' },
+    wifeDetails: { name: "lakshmi", imageUrl: '' },
+  }
+
+  useEffect(() => {
+    
+  }, [familyTree])
+
+  const add = () => {
+    const formvalue = form.getFieldsValue()
+    console.log(formvalue)
+  }
+
+  return (
+    <Space>
+      <Button type='primary' onClick={() => setIsModalOpen(true)}>Add Member</Button>
+      <Modal title="Add Family Members" open={isModalOpen} footer={null} onCancel={() => setIsModalOpen(false)}>
+        <Form form={form} onFinish={add}>
+          <Form.Item name='relation'>
+            <Select placeholder="Select Parent Name">
+              {parentList.map((value: string, index: number) =>
+                <Option>{value}</Option>
+              )}
+            </Select>
+          </Form.Item>
+          <Form.Item name='head'>
+            <Input placeholder='Name of the Head' />
+          </Form.Item>
+          <Form.Item name='wife'>
+            <Input placeholder='Wife of the Head' />
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>Add</Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Space>
+  );
 };
 
 export default App;
